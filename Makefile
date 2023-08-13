3e7722929abd93e9be2520d606e5379df41a748a
Eugene <e@egeek.me>
2023-08-13 14:42:24 +0300

    Test an XML tree processing function

--- Makefile
+++ Makefile
@@ -12,4 +12,4 @@ test:
 
 .PHONY:
 testd:
-	ghcid -c 'stack ghci --package={base,hxt,split,directory,filepath,text,css-text,time,process,hspec} --ghc-options=-hide-all-packages test.hs' -T main
+	ghcid -c 'stack ghci --package={base,hxt,split,directory,filepath,text,css-text,time,process,hspec,neat-interpolation} --ghc-options=-hide-all-packages test.hs' -T main
--- test.hs
+++ test.hs
@@ -1,11 +1,16 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-19.31 --package base,hxt,split,directory,filepath,text,css-text,time,process,hspec --ghc-options=-hide-all-packages
+-- stack script --resolver lts-19.31 --package base,hxt,split,directory,filepath,text,css-text,time,process,hspec,neat-interpolation --ghc-options=-hide-all-packages
+
+{-# LANGUAGE ImportQualifiedPost, QuasiQuotes #-}
 
 module Test where
 
 import Control.Monad
+import Data.Text qualified as T
 import Kitya hiding (main)
+import NeatInterpolation
 import Test.Hspec
+import Text.XML.HXT.Core
 
 main :: IO ()
 main = hspec spec
@@ -16,3 +21,49 @@ spec = do
     forM_ [("margin-left: 00px;", 0), ("margin-left: 120px;", 4)] $ \(style, level) ->
       it ("parses level from style " <> style) $
         levelFromStyle style `shouldBe` level
+
+  describe "removeBodyInComments" $
+    it "removes wrapping body tag in comments" $ do
+      let html = T.unpack [trimming|
+        <html>
+        <body>body
+          <div id="comm" class="comments">
+            <div class="comment">
+              <div class="comment_body">
+                <body>foo bar</body>
+              </div>
+            </div>
+            <div class="comment">
+              <div class="comment_body">
+                <body>второй</body>
+              </div>
+            </div>
+          </div>
+        </body>
+        </html>
+      |]
+
+      let expected = T.unpack [trimming|
+        <html>
+        <body>body
+          <div id="comm" class="comments">
+            <div class="comment">
+              <div class="comment_body">
+                foo bar
+              </div>
+            </div>
+            <div class="comment">
+              <div class="comment_body">
+                второй
+              </div>
+            </div>
+          </div>
+        </body>
+        </html>
+      |]
+
+      actual <- fmap head . runX $ readString [withParseHTML yes] html
+        >>> removeBodyInComments
+        >>> writeDocumentToString [withOutputXHTML, withAddDefaultDTD yes, withXmlPi no]
+
+      actual `shouldBe` expected
