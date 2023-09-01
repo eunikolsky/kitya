be7dca9a5e94ab6e07aa73b6ddd83cd7732e7ae9
Eugene <e@egeek.me>
2023-09-01 06:51:59 +0300

    Extract epub file into a dir

--- k.hs
+++ k.hs
@@ -1,5 +1,28 @@
 #!/usr/bin/env stack
 -- stack script --resolver lts-21.8
 
+import Control.Monad
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.Process
+
 main :: IO ()
-main = putStrLn "hello"
+main = getFile >>= extractEPUB
+
+getFile :: IO FilePath
+getFile = do
+  args <- getArgs
+  case args of
+    [file] -> pure file
+    _ -> die "k.hs <INPUT_EPUB>"
+
+-- EPUB processing
+
+-- | Extracts the epub file into a directory with the base name of the file.
+extractEPUB :: FilePath -> IO ()
+extractEPUB file = do
+  let dir = dropExtension file
+  out <- readProcess "unzip" ["-q", file, "-d", dir] ""
+  when (not $ null out) $
+    putStrLn $ mconcat ["extract ", file, ":\n", out]
