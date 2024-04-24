5f7fa6a984450f43ad1fc77fdcfbd3e59fbc260c
Eugene <e@egeek.me>
2024-04-24 20:28:05 +0300

    Parse map's title

--- map/map.hs
+++ map/map.hs
@@ -1,7 +1,29 @@
 #!/usr/bin/env stack
--- stack script --resolver=lts-22.18
+-- stack script --resolver=lts-22.18 --package=tagsoup --package=text
 
-{-# OPTIONS_GHC -Wall -Werror=missing-methods -Werror=missing-fields #-}
+{-# OPTIONS_GHC -Wall -Werror=missing-methods -Werror=missing-fields -Wprepositive-qualified-module #-}
+{-# LANGUAGE DerivingStrategies, OverloadedStrings #-}
+
+import Data.Text (Text)
+import Data.Text qualified as T (strip)
+import Data.Text.IO qualified as T (readFile)
+import System.Environment (getArgs)
+import Text.HTML.TagSoup (Tag(..), innerText, parseTags)
+
+-- | Information about a Kitya's track on the map.
+newtype Map = Map
+  { title :: Text
+  }
+  deriving newtype (Show)
+
+parseMapInfo :: FilePath -> IO Map
+parseMapInfo f = do
+  t <- T.readFile f
+  let tags = parseTags t
+      title = T.strip . innerText . take 2 . dropWhile (/= TagOpen "title" []) $ tags
+  pure Map{title}
 
 main :: IO ()
-main = putStrLn "map.hs"
+main = do
+  srcFile <- head <$> getArgs
+  print =<< parseMapInfo srcFile
